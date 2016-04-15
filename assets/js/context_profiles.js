2354dcca9485209daad99264a8f880500177e63a
Dieter Vanmoortel <dieter.vanmoortel@tobania.be>
2016-04-15 12:46:44 +0200

    First batch of coding standards fixes

--- assets/js/context_profiles.js
+++ assets/js/context_profiles.js
@@ -6,6 +6,7 @@
 (function ($, Drupal) {
 
   'use strict';
+
   /**
    * Highlights the block that was just placed into the block listing.
    *
@@ -18,12 +19,12 @@
     attach: function (context, settings) {
 
       $('.draggable-block').draggable({
-        appendTo: "#edit-regions",
-        connectToSortable: ".region-droppable",
-        revert: "invalid",
-        stack: ".draggable-block",
+        appendTo: '#edit-regions',
+        connectToSortable: '.region-droppable',
+        revert: 'invalid',
+        stack: '.draggable-block',
         scope: 'region-block',
-        stop: function( event, ui ) {
+        stop: function (event, ui) {
           var region = $(this).parents('.region-droppable');
           Drupal.behaviors.contextProfiles.adjustBlockWeights(region);
         }
@@ -31,32 +32,32 @@
 
       $('.region-droppable').droppable({
         addClasses: false,
-        tolerance: "pointer",
+        tolerance: 'pointer',
         accept: '.draggable-block',
         scope: 'region-block',
-        hoverClass: "region-active",
-        drop: function(event, ui) {
+        hoverClass: 'region-active',
+        drop: function (event, ui) {
           var region = $(this).attr('id');
           ui.draggable.find('.block-region').attr('value', region);
         },
-        out: function(event, ui) {
+        out: function (event, ui) {
           ui.draggable.find('.block-region').attr('value', '');
         }
       }).sortable({
-        items: ".block-form",
+        items: '.block-form',
         cancel: '.sortable-block'
       });
 
-      $('.reset-block').on('click', function(e) {
+      $('.reset-block').on('click', function (e) {
         $(this).parent().find('input').attr('value', '');
         var plugin = $(this).parent().attr('plugin');
         var parent = $('#edit-disabled').find('#wrap-' + plugin);
         Drupal.behaviors.contextProfiles.moveBlock($(this).parent(), parent);
       });
 
-      $( "#edit-block-lookup" ).keyup(function() {
+      $("#edit-block-lookup").keyup(function () {
         var lookup = $('#edit-block-lookup').val().toLowerCase();
-        $.each($('#edit-disabled').find('.block-form'), function(){
+        $.each($('#edit-disabled').find('.block-form'), function () {
           if ($(this).find('label').html().toLowerCase().search(lookup) > -1) {
             $(this).fadeIn();
           }
@@ -67,16 +68,16 @@
       });
 
     },
-    moveBlock: function ( $item , $parent) {
-      $item.fadeOut(200, function() {
+    moveBlock: function ($item, $parent) {
+      $item.fadeOut(200, function () {
         $item
-          .appendTo( $parent )
+          .appendTo($parent)
           .fadeIn();
       });
     },
-    adjustBlockWeights: function( region ) {
+    adjustBlockWeights: function (region) {
       var index = -10;
-      region.find('.block-weight').each(function() {
+      region.find('.block-weight').each(function () {
         $(this).val(index);
         index++;
       })
