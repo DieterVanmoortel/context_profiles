32aa22d82563ed563ea15a489f412ed48721ab2f
Dieter Vanmoortel <dieter.vanmoortel@tobania.be>
2016-04-18 09:04:04 +0200

    WIP: Added Unit test to check presence of local task

---
+++ tests/src/Unit/ContextProfilesLocalTaskTest.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\context_profiles|ContextProfilesLocalTaskTest.
+ */
+
+namespace Drupal\Tests\context_profiles\Unit;
+
+use Drupal\node\Entity\NodeType;
+use Drupal\simpletest\WebTestBase;
+
+/**
+ * Tests that local task links are available for context profiles.
+ *
+ * @group context_profiles
+ */
+class ContextProfilesLocalTaskTest extends WebTestBase {
+
+  /**
+   * The bundle being tested.
+   *
+   * @var string
+   */
+  protected $bundle;
+
+  /**
+   * The content type being tested.
+   *
+   * @var NodeType
+   */
+  protected $contentType;
+
+  /**
+   * The 'webmaster' user to use during testing.
+   *
+   * @var \Drupal\user\UserInterface
+   */
+  protected $webmaster;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = array('context', 'context_profiles', 'node', 'block_content', 'devel');
+
+  /**
+   * The profile to install as a basis for testing.
+   *
+   * @var string
+   */
+  protected $profile = 'standard';
+
+  protected function setUp() {
+    parent::setUp();
+
+    // Create a content type.
+    $this->bundle = $this->randomMachineName();
+    $this->contentType = $this->drupalCreateContentType(array('type' => $this->bundle));
+
+    // Create a webmaster user.
+    $permissions = array(
+      'use context profile ui',
+      'administer nodes',
+      "edit any $this->bundle content",
+    );
+    $this->webmaster = $this->drupalCreateUser($permissions);
+  }
+
+  /**
+   * Tests that a contextual link is available for context profiles.
+   */
+  public function testLocalTasks() {
+    $this->drupalLogin($this->webmaster);
+
+    // Create a node.
+    $title = $this->randomString();
+    $node = $this->drupalCreateNode(array('type' => $this->bundle, 'title' => $title));
+
+    $html = $this->drupalGet('node/' . $node->id());
+    $this->assertResponse('200');
+
+    $this->assertRaw('Blocks');
+//    // Check that the context profiles link appears on the node page.
+    $this->assertLinkByHref('/node/' . $node->id() .'/blocks');
+//
+//    $this->drupalGet('node/' . $node->id() . '/blocks');
+//    $this->assertResponse('200');
+//
+//    $this->assertRaw('Active contexts');
+
+  }
+
+}
